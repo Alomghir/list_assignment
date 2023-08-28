df15d95666aad33eb5b34ce12d3db0e2da573b14
Alomghir Hossain <142247486+Alomghir@users.noreply.github.com>
2023-08-28 20:38:59 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+#question 1
+
+fruits_list=['apple','banana','cherry']
+
+fruits_list.append('orange')
+print(fruits_list)
+
+
+#question 2
+
+number_list=[1,2,3,4,5]
+number_list.remove(3)
+print(number_list)
+
+
+#question 3
+
+letters_list=['a','b','c','a','d']
+count=letters_list.count('a')
+print(count)
+
+
